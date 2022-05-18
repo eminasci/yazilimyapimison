b4b14f30d69f237d59fe115f7a8b13dabfd60245
ASEM ALSALAHI <96667066+ASEMALSALAHI@users.noreply.github.com>
2022-05-18 16:26:18 +0300

    Add files via upload

---
+++ sonuc.sql
@@ -0,0 +1,21 @@
+﻿USE [yazılımyapımı]
+GO
+
+/****** Object:  Table [dbo].[sonuc]    Script Date: 18.05.2022 16:11:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[sonuc](
+	[ogrenci_id] [int] NOT NULL,
+	[dogru_soru_sayisi] [int] NOT NULL,
+	[yanlis_soru_sayisi] [int] NOT NULL,
+	[bos_soru_sayisi] [int] NOT NULL,
+	[toplam_puan] [float] NULL,
+	[ogrenci_adi] [nchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+
+
