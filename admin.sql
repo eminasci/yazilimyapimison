7f58ee9bbc63a12393fdc066f59541aa45b55db8
ASEM ALSALAHI <96667066+ASEMALSALAHI@users.noreply.github.com>
2022-05-18 16:23:47 +0300

    Add files via upload

---
+++ admin.sql
@@ -0,0 +1,17 @@
+﻿USE [yazılımyapımı]
+GO
+
+/****** Object:  Table [dbo].[admin]    Script Date: 18.05.2022 16:03:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[admin](
+	[adminemail] [nvarchar](50) NOT NULL,
+	[adminsifre] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+
+
