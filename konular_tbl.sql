13975bdbd38cbe51bd5fe62e0d2a57a444987dfd
ASEM ALSALAHI <96667066+ASEMALSALAHI@users.noreply.github.com>
2022-05-18 16:24:13 +0300

    Add files via upload

---
+++ konular_tbl.sql
@@ -0,0 +1,21 @@
+﻿USE [yazılımyapımı]
+GO
+
+/****** Object:  Table [dbo].[konular_tbl]    Script Date: 18.05.2022 16:06:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[konular_tbl](
+	[Id] [int] NOT NULL,
+	[konu_adi] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_konular_tbl] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
